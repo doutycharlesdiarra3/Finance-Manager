76b68c2fa4abea6a6d988399dff7bcba79757e4e
doutycharlesdiarra3 <doutycharlesdiarra3.com>
2026-01-09 10:30:07 +0000

    Mofication dans setting

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")  # clé locale si non définie
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
